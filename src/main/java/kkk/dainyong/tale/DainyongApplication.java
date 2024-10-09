1f273dfc003ca3638c2486e932acba8109e7bdd2
dae <cys964@gmail.com>
2024-10-10 03:38:30 +0900

    매퍼스캔

--- src/main/java/kkk/dainyong/tale/DainyongApplication.java
+++ src/main/java/kkk/dainyong/tale/DainyongApplication.java
@@ -1,9 +1,11 @@
 package kkk.dainyong.tale;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("kkk.dainyong.tale.repository")
 public class DainyongApplication {
 
 	public static void main(String[] args) {
