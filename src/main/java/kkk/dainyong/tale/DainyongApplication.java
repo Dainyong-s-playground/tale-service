58ac52f3d63d0d6539dced6015b3fbdd369e54c2
Beomky <nobleman1214@naver.com>
2024-10-09 23:56:32 +0900

    Initial Setting

---
+++ src/main/java/kkk/dainyong/tale/DainyongApplication.java
@@ -0,0 +1,13 @@
+package kkk.dainyong.tale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainyongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainyongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kkk/dainyong/tale/DainyongApplicationTests.java
@@ -0,0 +1,13 @@
+package kkk.dainyong.tale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DainyongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
