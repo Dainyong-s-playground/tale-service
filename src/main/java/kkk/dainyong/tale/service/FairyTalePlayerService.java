4006c2f7d6845c67167094e19cb391db3e51fd41
Chani17 <cucute8878@naver.com>
2024-10-11 03:14:19 +0900

    Remove: Unnecessary Slf4j import

--- src/main/java/kkk/dainyong/tale/service/FairyTalePlayerService.java
+++ src/main/java/kkk/dainyong/tale/service/FairyTalePlayerService.java
@@ -4,7 +4,6 @@ import kkk.dainyong.tale.model.dto.FairyTaleDataDTO;
 import kkk.dainyong.tale.repository.FairyTaleDataRepository;
 import kkk.dainyong.tale.repository.FairyTaleRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +19,7 @@ public class FairyTalePlayerService {
     public FairyTaleDataDTO getFairyTaleData(Long fairyTaleNumber) {
         // 존재하는 동화인지 체크
         if(fairyTaleRepository.findById(fairyTaleNumber) == null) throw new IllegalStateException("존재하지 않는 동화책입니다.");
-        
+
         return fairyTaleDataRepository.getFairyTaleData(fairyTaleNumber);
     }
 }
