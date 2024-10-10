5fb40821fc0f86146e0746bcb44087f6a24c5e5f
dae <cys964@gmail.com>
2024-10-10 20:54:54 +0900

    HistoryRepository

---
+++ src/main/java/kkk/dainyong/tale/repository/HistoryRepository.java
@@ -0,0 +1,19 @@
+package kkk.dainyong.tale.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import kkk.dainyong.tale.model.History;
+
+@Mapper
+public interface HistoryRepository {
+	List<History> getRecentlyWatchedContent(@Param("profileId") Long profileId);
+
+	void insertHistory(History history);
+
+	void updateHistory(History history);
+
+	void deleteHistory(@Param("profileId") Long profileId, @Param("fairyTaleId") Long fairyTaleId);
+}
