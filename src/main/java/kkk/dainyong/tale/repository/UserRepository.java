4ae92fb12cbfd0b4c1d6aa74dee19d415ee9a823
dae <cys964@gmail.com>
2024-10-15 03:12:20 +0900

    유저 레포지토리

---
+++ src/main/java/kkk/dainyong/tale/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package kkk.dainyong.tale.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface UserRepository {
+	int getUserCredit(@Param("userId") String userId);
+
+	void updateUserCredit(@Param("userId") String userId, @Param("credit") int credit);
+}
