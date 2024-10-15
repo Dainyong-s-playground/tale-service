6a2a797ccee17759a8e3ee5d6199da1753901145
Chani17 <cucute8878@naver.com>
2024-10-15 22:43:44 +0900

    Feat: insert/update preferences count

---
+++ src/main/java/kkk/dainyong/tale/model/dto/UpdatePreferenceDTO.java
@@ -0,0 +1,16 @@
+package kkk.dainyong.tale.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePreferenceDTO {
+    private Long profileId;
+    private Long tagId;
+    private int count;
+}
